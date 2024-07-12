8ed1a9ecafaa3f85fe57c304a25f615d95784219
sontiaemerson <sontiae21@gmail.com>
2024-07-12 13:36:04 +0100

    modules

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.23;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script {
+    NetWorkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
+    struct  NetWorkConfig {
+        address priceFeed;
+    }
+
+    constructor() {
+        if(block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+
+        } else {
+            activeNetworkConfig = getorCreatAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetWorkConfig memory) {
+        NetWorkConfig memory sepoliaConfig = NetWorkConfig({priceFeed:0x694AA1769357215DE4FAC081bf1f309aDC325306});
+        return sepoliaConfig;
+    }
+    function getorCreatAnvilEthConfig() public returns (NetWorkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0) ) {
+            return activeNetworkConfig;
+        }
+
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPricefeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        vm.stopBroadcast();
+
+        NetWorkConfig memory anvilConfig = NetWorkConfig({priceFeed: address(mockPricefeed)});
+        return anvilConfig;
+    }
+}
